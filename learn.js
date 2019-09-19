55cdf0d7548d9fceb8f6ef42b60f8461a2299d88
Charles Glass <charlie-glass@live.com>
2019-09-19 09:29:19 -0700

    added confirm

---
+++ learn.js
@@ -0,0 +1,10 @@
+var answer = confirm('Do you want to learn about alpaca?');
+var response;
+
+if(answer=== true) {
+response = 'Of course you do, get to it!'
+} else {
+    response = 'whatcha doing here then?'
+}
+
+document.write('<h3>' + greeting + '</h3>');
