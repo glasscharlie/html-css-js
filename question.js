b4f60ed1db9702201d91b5219818a4e9a5c83943
Charles Glass <charlie-glass@live.com>
2019-09-19 09:09:24 -0700

    changed picture size

--- question.js
+++ question.js
@@ -2,7 +2,7 @@ var answer = prompt('Do you think alpacas are cute?');
 
 var message;
 if(answer=== 'yes') { message = 'Correct, alpacas are cute.' 
-} else if (answer=== 'no') { message = 'Who do you think you are'
+} else if (answer=== 'no') { message = 'Who do you think you are?'
 }
 else {message = 'Alpacas are very cute!'
 }
