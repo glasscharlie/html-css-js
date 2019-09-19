9d0d10cbba3d6d3ea075dd189b174b7379f6793e
Charles Glass <charlie-glass@live.com>
2019-09-18 22:38:42 -0700

    added js

---
+++ question.js
@@ -0,0 +1,10 @@
+var answer = prompt('Do you think alpacas are cute?');
+
+var message;
+if(answer=== 'yes') { message = 'Correct, alpacas are cute.' 
+} else if (answer=== 'no') { message = 'Who do you think you are'
+}
+else {message = 'Alpacas are very cute!'
+}
+
+document.write('<h3>' + message + '<h3>');
---
+++ time.js
@@ -0,0 +1,11 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Perfect evening for alpacas!';
+} else if (hourNow > 12) {greeting = 'Perfect afternoon for alpacas'
+} else if (hourNow > 0) {greeting = 'Perfect morning for alpacas'
+}
+
+document.write('<h3>' + greeting + '</h3>');
